a55d2d8168f632569aee2f3d0dc091ca74e73b07
Donovan McKelvey <donovan@tempo.ai>
2013-08-27 14:19:08 -0700

    initial base commit for client outline

---
+++ client/src/Client.java
@@ -0,0 +1,18 @@
+public class Client {
+	public static void main(String[] args) {
+		// look for settings file
+		// if exists, load file
+		// else, make file and setup new account
+
+		// make connection
+
+		// if already have work, check with server if work is still valid
+		// if no work or if work is invalid, get work
+
+		// do work (in background)
+
+		// send work
+
+		// check for new work (loop)
+	}
+}
