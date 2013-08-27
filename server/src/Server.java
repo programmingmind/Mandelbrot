6b351b6fdf5a6ee4014770435b1992d4ef153ef9
Donovan McKelvey <donovan@tempo.ai>
2013-08-27 16:12:16 -0700

    inital server ideas

---
+++ server/src/Server.java
@@ -0,0 +1,16 @@
+public class Server {
+	public static void main(String[] args) {
+		// look for resource files, making them if they don't exist
+
+		// start up web server on specific port
+
+		// wait for connections....
+
+		// on connection fork off
+		// validate user to be able to track their work
+		// either validate working data set, send data, or accept new data
+		// credit is # of total iterations between all points
+
+		// background thread to handle file I/O and getting new requests
+	}
+}
