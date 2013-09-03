2a60a7deb1fa8abf649e468a37bfe7071dd0d236
Donovan McKelvey <donovan@tempo.ai>
2013-09-03 10:51:59 -0700

    outlining client code in a bit more detail

--- client/src/Client.java
+++ client/src/Client.java
@@ -1,20 +1,48 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Client {
 	public static void main(String[] args) {
-		// look for settings file
-		// if exists, load file
-		// else, make file and setup new account
+		String homeDir = System.getProperty("user.home");
+		if (homeDir == null) {
+			System.err.println("Can't find home directory. Using current directory instead");
+			File currentDir = new File("");
+			try {
+				homeDir = currentDir.getCanonicalPath();
+			}
+			catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if (!homeDir.endsWith(File.separator))
+			homeDir = homeDir + File.separator;
 
-		// make connection
+		// get user login name
+		// verify/create new user here and verify work is still valid
 
-		// if already have work, check with server if work is still valid
-		// if no work or if work is invalid, get work
+		File settingsFile = new File(homeDir + "Mandelbrot.settings");
+		if (settingsFile.exists()) {
+			// load file and settings
+		}
+		else {
+			// create file and fill with default settings
+		}
 
-		// do work (in background)
-		// Thread worker = new Thread(new Worker());
-		// worker.start();
+		do {
+			// if no work, get work
+			// start work in background
 
-		// send work
+			// Thread worker = new Thread(new Worker());
+			// worker.start();
 
-		// check for new work (loop)
+			// prompt user for status/quit/view total work done/etc
+
+			// when work ends, stop prompting and send work, loop
+		}
+		while (!quit);
+
+		// save current status of work to disk
+
 	}
 }
